2a639272429f3fc9b1826a150a1f00c6da751610
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-12-19 15:02:50 -0500

    Made initial form fields and basic allignment fixes

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem  extends StatefulWidget{
   const NewItem({super.key});
@@ -13,7 +15,50 @@ class _NewItemState extends State<NewItem>{
       appBar: AppBar(
         title: Text("Add a new item..."),
       ),
-      body: Padding(padding: EdgeInsets.all(12)),
+      body: Padding(padding: EdgeInsets.all(12),
+      child: Form(
+        child: Column(children: [
+          TextFormField(
+            maxLength: 50,
+            decoration: InputDecoration(label: Text('Name')),
+            validator: (value){
+              return null;
+            },
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  label: Text("Quantity"),
+                ),
+                initialValue: '1',
+              ),
+            ),
+            const SizedBox(width:8),
+            Expanded(
+              child: DropdownButtonFormField(items:[
+                for(final category in categories.entries)
+                DropdownMenuItem(
+                  value:  category.value,
+                  child: Row(children: [
+                  Container(
+                    width: 16,
+                    height: 16,
+                    color: category.value.color
+                  ),
+                  const SizedBox(width: 6),
+                  Text(category.value.title),
+                ],)
+                ,)
+              ], onChanged: (value){}),
+            )
+          ],)
+        ],
+        )
+        )
+        ),
     );
   }
 }
