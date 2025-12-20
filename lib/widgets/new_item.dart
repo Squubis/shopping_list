7a1196c1570e9b9239c912c18b9eea9d19f8bfda
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-12-19 22:48:54 -0500

    added a loading spinner when its sending and preventing resending again

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -18,10 +18,14 @@ class _NewItemState extends State<NewItem>{
   var _enteredName='';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
   
   void _saveItem()async{
     if(_formKey.currentState!.validate()){
       _formKey.currentState!.save();
+      setState(() {
+        _isSending=true;
+      });
       final url = Uri.https('shoppinglist-e1fe9-default-rtdb.firebaseio.com', 'shoppinglist.json');
       final response = await http.post(url,
       headers: {'Content-Type':'application/json'},
@@ -114,8 +118,8 @@ class _NewItemState extends State<NewItem>{
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              TextButton(onPressed: () {_formKey.currentState!.reset();} , child: const Text("Reset")),
-              ElevatedButton(onPressed: _saveItem, child: const Text("Add Item")),
+              TextButton(onPressed:_isSending ? null: () {_formKey.currentState!.reset();} , child: const Text("Reset")),
+              ElevatedButton(onPressed:_isSending?null: _saveItem, child: _isSending ? SizedBox(height: 16, width: 16,child: CircularProgressIndicator(),): Text("Add Item")),
           ],)
         ],
         )
